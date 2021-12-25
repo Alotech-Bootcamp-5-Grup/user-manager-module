43dee2315b8faedbd4f7f50fd05c821ebace35fc
eren <erentnr@yandex.com>
2021-12-25 03:26:11 +0300

    Add prot variable to .env file

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 // Generate tables and stored procedures
 db.sequelize.sync()
